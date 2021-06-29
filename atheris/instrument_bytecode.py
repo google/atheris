4a600bb365f1bee741dd23e6dbaf7fff939f82db
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2021-06-29 15:53:23 +0200

    Added copyright notice for Fraunhofer FKIE and updated notices to the current year

--- atheris/instrument_bytecode.py
+++ atheris/instrument_bytecode.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Fraunhofer FKIE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 This module provides the instrumentation functionality for atheris.
 Mainly the function patch_code(), which can instrument a code object
@@ -208,7 +222,7 @@ class Instrumentor:
         6. Call _handle_size_changes()
     Take a look at trace_control_flow() and trace_data_flow() for examples.
     
-    Note that this calls only supports insertions, not deletions.
+    Note that Instrumentor only supports insertions, not deletions.
     """
     def __init__(self, code, start_idx, start_pc):
         self._start_idx = start_idx
