c98cc23cb25d87e727d16afe5cd64a3c50993f58
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2021-07-08 18:25:31 +0200

    Updated copyright info

--- example_fuzzers/fuzzing_example.py
+++ example_fuzzers/fuzzing_example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2021 Google LLC
+# Copyright 2020 Google LLC
 # Copyright 2021 Fraunhofer FKIE
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
