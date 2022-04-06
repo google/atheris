5fce19b217ddc06d6b36628d1d93eb5103fb1b92
Googler <atheris@google.com>
2022-04-06 15:07:12 -0400

    Add script to run tests. This is slightly complicated because said tests live in-tree but import atheris by name, so in the current state we have to automatically set up tmp dir, copy everything there, and set up a venv to install atheris in in order to run them. That dir is removed at the end of the run.
    Unfortunately there are a ton of logs here, so I verified that this method will summarize any failures at the end, even if they happen in multiple separate tests.
    
    Add venv stuff go .gitignore as well. I don't think will be problematic for PyPI pushes because the deploy_pypi.sh script checks for gitignored files.
    
    PiperOrigin-RevId: 439901915
    Change-Id: I4829910b011e452bd42ae62cf3385de08156ca71

---
+++ run_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ "$(python3 -m venv 2>&1)" =~ "No module named venv" ]]; then
+  echo "Virtualenv not installed. This script requires virtual env, please install it."
+  exit 1;
+fi
+
+# Set up temp dir containing atheris and cd into it.
+cd "$( dirname ${BASH_SOURCE[0]} )"
+TMP_DIR="$(mktemp -d)"
+cp -r . "${TMP_DIR?}"
+cd "${TMP_DIR?}"
+
+# Set up virtual env
+python3 -m venv .
+source bin/activate
+python -m pip install .
+python -m pip install PyInstaller
+
+cd src && python3 -m unittest discover . -p '*_test.py'
+rm -rf "$TMP_DIR"
