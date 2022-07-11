14a148e5cd831eac16c76e32371ba0d9e4735269
Googler <atheris@google.com>
2022-07-11 12:47:52 -0400

    Updates deploy_pypi.sh so that it automates the full deployment process.
    Specifically:
    
    - When no env is specified (via argv), deploy to testpypi first, then download and run tests, if they pass then deploy to pypi.
    - Automatically run tests after downloading from testpypi.
    - Automatically check there have been no local modifications to any files before uploading to pypi.
    - Refactored some common functionality to functions.
    
    Testing:
    
    - that the happy path succeeds by prepending echo to the twine commands.
    - that it fails when the tests don't pass by replacing run_tests.sh.
    
    PiperOrigin-RevId: 460235597
    Change-Id: If303ce882c057f0613f3a70e09eb933a2255d74c

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,11 @@ if [[ "$(python3 -m venv 2>&1)" =~ "No module named venv" ]]; then
 fi
 
 # Set up temp dir containing atheris and cd into it.
-cd "$( dirname ${BASH_SOURCE[0]} )"
+SRC_DIR="$1"
+if [ -z "$SRC_DIR" ]; then
+  SRC_DIR="$( dirname ${BASH_SOURCE[0]} )"
+fi
+cd "$SRC_DIR"
 TMP_DIR="$(mktemp -d)"
 cp -r . "${TMP_DIR?}"
 cd "${TMP_DIR?}"
