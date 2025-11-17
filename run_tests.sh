4efb5832717214c7a7bd3d72536168c77286e3ba
Ian Eldred Pudney <puddles@google.com>
2025-11-17 10:16:55 -0800

    Correct copy-paste mistake in run_tests.sh
    The previous change to this script inadvertently included some extra content copy-pasted from my terminal, including the prompt line itself.
    
    PiperOrigin-RevId: 833393384
    Change-Id: I8d12eec3346c6283e28361e3d15dc5ec516325fb

--- run_tests.sh
+++ run_tests.sh
@@ -23,35 +23,6 @@ source bin/activate
 python -m pip install .
 python -m pip install PyInstaller
 
-cd src && python -m unittest discover . -p '*_test.py'
-rm -rf "$TMP_DIR"
-
- atherisversionautomation  google3/third_party/py/atheris  cat ~/atheris/run_tests.sh                                                   ipudney@ipudney
-#!/bin/bash
-
-# Make sure we propagate exit codes, for kokoro.
-set -e
-
-if [ -z "$PYTHON" ]; then
-  PYTHON="python3"
-fi
-
-# Set up temp dir containing atheris and cd into it.
-SRC_DIR="$1"
-if [ -z "$SRC_DIR" ]; then
-  SRC_DIR="$( dirname ${BASH_SOURCE[0]} )"
-fi
-cd "$SRC_DIR"
-TMP_DIR="$(mktemp -d)"
-cp -r . "${TMP_DIR?}"
-cd "${TMP_DIR?}"
-
-# Set up virtual env
-"$PYTHON" -m virtualenv .
-source bin/activate
-python -m pip install .
-python -m pip install PyInstaller
-
 echo "--- Starting Python Test Suite ---"
 echo "Searching recursively for files matching *_test.py"
 
